baf57278f7fc5857c5a3622e13585238ca2b3abd
Joe <joe@joebailey.xyz>
2019-12-23 00:04:01 +0000

    Form updates for Netlify

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,67 +1,92 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './css/footer.scss';
-import $ from 'jquery';
+
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { FName: "", LName: "", Email: "", Telephone: "", Subject: "", Message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { FName, LName, Email, Telephone, Subject, Message } = this.state;
+    return (
+      <form className="Contact" method="post" name="contact" id="contact" onSubmit={this.handleSubmit}>
+        <input type="hidden" name="form-name" value="contact"/>
+        <label htmlFor="FName">Name:</label>
+        <input id="fname" name="FName" type="text" placeholder="Han" required value={FName} onChange={this.handleChange}></input>
+        <label htmlFor="LName">Last Name:</label>
+        <input id="lname" name="LName" type="text" placeholder="Solo" required value={LName} onChange={this.handleChange}></input>
+        <label htmlFor="Email">Email:</label>
+        <input id="mail" name="Email" type="email" placeholder="millenium@falcon.net" required value={Email} onChange={this.handleChange}></input>
+        <label htmlFor="Telephone">Telephone:</label>
+        <input id="telephone" name="Telephone" type="tel" placeholder="+447569836548" required value={Telephone} onChange={this.handleChange}></input>
+        <label htmlFor="Subject">Subject:</label>
+        <input id="subject" name="Subject" type="text" placeholder="Corellia" required value={Subject} onChange={this.handleChange}></input>
+        <label htmlFor="Message">Message:</label>
+        <textarea id="message" name="Message" placeholder="Chewie, the exhaust ports need cleaning" required value={Message} onChange={this.handleChange}></textarea>
+        <div id="SubmitContainer">
+          <input id="submit" type="submit" name="Submit" className="Button ColorTwo" value="submit"></input>
+        </div>
+      </form>
+    );
+  }
+}
 
 function Footer() {
-    return (
-        <footer className="GlobalFooter">
-          <div className="FooterContainer SpaceBackground">
-            <div className="FooterContactContainer">
-              <div className="FooterContact">
-                <h2>Get in Touch</h2>
-                <p className="FooterContactInfo">We’d love to help you on your next big project. We understand what it means to dream big, and then achieve that dream.</p>
-                <form className="Contact" method="post" name="contact" id="contact">
-                  <input type="hidden" name="form-name" value="contact"/>
-                  <label htmlFor="fname">Name:</label>
-                  <input id="fname" name="FName" type="text" placeholder="Han" required></input>
-                  <label htmlFor="lname">Last Name:</label>
-                  <input id="lname" name="LName" type="text" placeholder="Solo" required></input>
-                  <label htmlFor="email">Email:</label>
-                  <input id="email" name="Email" type="email" placeholder="millenium@falcon.net" required></input>
-                  <label htmlFor="telephone">Telephone:</label>
-                  <input id="telephone" name="Telephone" type="tel" placeholder="+447569836548" required></input>
-                  <label htmlFor="subject">Subject:</label>
-                  <input id="subject" name="Subject" type="text" placeholder="Corellia" required></input>
-                  <label htmlFor="message">Message:</label>
-                  <textarea id="message" name="Message" placeholder="Chewie, the exhaust ports need cleaning" required></textarea>
-                  <div id="SubmitContainer">
-                    <input id="submit" type="submit" name="submit" className="Button ColorTwo" value="submit"></input>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div className="FooterBottomContainer">
-              <div className="FooterBottom">
-                <div className="FooterSitemap">
-                  <h2>Galexia</h2>
-                  <Link className="FooterNavLink" id="WorkWithUs" to="/Work-With-Us">Work with us</Link>
-                  <Link className="FooterNavLink" id="Privacy" to="/Privacy-Policy">Privacy Policy</Link>
-                  <Link className="FooterNavLink" id="Cookie" to="/Cookie-Policy">Cookie Policy</Link>
-                  <Link className="FooterNavLink" id="Terms" to="/Terms">Terms & Conditions</Link>
-                </div>
-                <div className="FooterTestimonials">
-                  <p className="Testimonial">“Galexia helped my brand realize it’s potential. They gave me valuable content to share with my clients”</p>
-                  <p className="TestimonialCompany">- A Nice Company</p>
-                </div>
-              </div>
-              <div className="FooterCopyright">
-                <p>© {new Date().getFullYear()} Galexia. Company No: 11824756.</p>
-              </div>
+  return (
+      <footer className="GlobalFooter">
+        <div className="FooterContainer SpaceBackground">
+          <div className="FooterContactContainer">
+            <div className="FooterContact">
+              <h2>Get in Touch</h2>
+              <p className="FooterContactInfo">We’d love to help you on your next big project. We understand what it means to dream big, and then achieve that dream.</p>
+              <ContactForm />
             </div>
           </div>
-          {
-            $("#contact").submit(function(e) {
-              e.preventDefault();
-              var $form = $(this);
-              $.post($form.attr("action"), $form.serialize()).then(function() {
-              alert("Thank you!");
-              $form[0].reset();
-              });
-            })
-          }
-        </footer>
-    )
+          <div className="FooterBottomContainer">
+            <div className="FooterBottom">
+              <div className="FooterSitemap">
+                <h2>Galexia</h2>
+                <Link className="FooterNavLink" id="WorkWithUs" to="/Work-With-Us">Work with us</Link>
+                <Link className="FooterNavLink" id="Privacy" to="/Privacy-Policy">Privacy Policy</Link>
+                <Link className="FooterNavLink" id="Cookie" to="/Cookie-Policy">Cookie Policy</Link>
+                <Link className="FooterNavLink" id="Terms" to="/Terms">Terms & Conditions</Link>
+              </div>
+              <div className="FooterTestimonials">
+                <p className="Testimonial">“Galexia helped my brand realize it’s potential. They gave me valuable content to share with my clients”</p>
+                <p className="TestimonialCompany">- A Nice Company</p>
+              </div>
+            </div>
+            <div className="FooterCopyright">
+              <p>© {new Date().getFullYear()} Galexia. Company No: 11824756.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+  )
 } 
 
 export default Footer;
