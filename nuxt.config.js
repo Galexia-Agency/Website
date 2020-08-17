2a8df7f71f74e5671cdb3fe58bb7018dbae3d6d7
Joe <joe@joebailey.xyz>
2020-08-17 01:14:38 +0100

    Updated manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -188,55 +188,55 @@ export default {
           src: '/android-chrome-36x36.png?v=dLXLEPEjpj',
           sizes: '36x36',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-48x48.png?v=dLXLEPEjpj',
           sizes: '48x48',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-72x72.png?v=dLXLEPEjpj',
           sizes: '72x72',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-96x96.png?v=dLXLEPEjpj',
           sizes: '96x96',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-144x144.png?v=dLXLEPEjpj',
           sizes: '144x144',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-192x192.png?v=dLXLEPEjpj',
           sizes: '192x192',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-256x256.png?v=dLXLEPEjpj',
           sizes: '256x256',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-384x384.png?v=dLXLEPEjpj',
           sizes: '384x384',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         },
         {
           src: '/android-chrome-512x512.png?v=dLXLEPEjpj',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'maskable'
+          purpose: 'any maskable'
         }
       ]
     },
