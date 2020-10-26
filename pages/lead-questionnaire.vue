a9dfde314e6f8ab60aab278733b0d938c755ee90
Joe <joe@joebailey.xyz>
2020-10-26 23:01:32 +0000

    Added lead questionnaire

---
+++ pages/lead-questionnaire.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="typeform-widget" data-url="https://form.typeform.com/to/OGff6UOz" style="width: 100%; height: 90vh" />
+</template>
+
+<script>
+export default {
+  created () {
+    this.cookie(false)
+  },
+  beforeDestroy () {
+    this.cookie(true)
+  },
+  methods: {
+    cookie (display) {
+      if (process.client) {
+        require('../assets/js/typeform')
+        let css
+        if (display === true) {
+          css = `
+            .cookieControl {
+              display: block
+            }
+          `
+        } else {
+          css = `
+            .cookieControl {
+              display: none
+            }
+          `
+        }
+        const head = document.head
+        const style = document.createElement('style')
+        head.appendChild(style)
+        style.type = 'text/css'
+        style.appendChild(document.createTextNode(css))
+      }
+    }
+  },
+  head () {
+    return {
+      title: 'Lead Questionnaire',
+      meta: [
+        { hid: 'robots', name: 'robots', content: 'noindex' }
+      ],
+      link: [
+        { hid: 'canonical', rel: 'canonical', href: 'https://galexia.agency/lead-questionnaire/' }
+      ]
+    }
+  }
+}
+</script>
