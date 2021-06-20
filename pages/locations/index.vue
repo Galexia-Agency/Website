4c99bc78763c1ba59bcea73694ceccedc72df968
Joe <joe@joebailey.xyz>
2021-06-20 12:34:44 +0100

    Removed some locations pages

--- pages/locations/index.vue
+++ pages/locations/index.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
-    <nuxt-link to="/locations/trumpington" />
-    <nuxt-link to="/locations/bassingbourn" />
     <nuxt-link to="/locations/cambridge" />
-    <nuxt-link to="/locations/cambridgeshire" />
 
     <nuxt-link to="/locations/hertfordshire" />
     <nuxt-link to="/locations/royston" />
-    <nuxt-link to="/locations/melbourne" />
-    <nuxt-link to="/locations/baldock" />
-    <nuxt-link to="/locations/letchworth" />
     <nuxt-link to="/locations/stevenage" />
 
     <nuxt-link to="/locations/southampton" />
